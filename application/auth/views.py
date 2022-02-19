1cfbb8ff1854ef542fe2f44a56a5ef9cadd22a3f
Subkhon Amrulloev <amrulloev.suhon@gmail.com>
2022-02-19 16:41:34 +0900

    flask login implemented. using user_base with verify_password functionality

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,9 +1,9 @@
 from . import auth_bp
 from .forms import RegisterForm, LoginForm
-from flask_login import login_user, login_required
-from flask import render_template, flash, redirect, url_for
+from flask_login import login_user, login_required,logout_user,current_user
+from flask import render_template, flash, redirect, url_for,request
 from .models import User, db
-
+from application import login_manager
 
 @auth_bp.route("/register", methods=["GET", "POST"])
 def register():
@@ -25,7 +25,22 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and user.verify_password(form.password.data):
             login_user(user)
+            flash(f"Logged in {user} successfully","success")
+            next=request.args.get("next")
+            if next is not None:
+                return redirect(next)
+            else:
+                return redirect(url_for("auth_bp.users"))
     return render_template("common_form_render.html", form=form)
+
+@auth_bp.route("/logout",methods=["GET"])
+def logout():
+    logout_user()
+    return redirect(url_for("main_bp.home"))
+
+@login_manager.unauthorized_handler
+def unauthorized_callback():
+    return redirect(url_for('auth_bp.login', next=request.path))
 
 
 @auth_bp.route("/users", methods=["GET", "POST"])
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from application import create_app
 import os
+from application import db
+from application.main.models import Product,Category
+from application.auth.models import User
+from flask_migrate import Migrate
 
 app = create_app(os.environ.get("FLASK_ENV") or "development")
+migrate = Migrate(app, db)
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User)
 
 if __name__ == "__main__":
     app.run()
